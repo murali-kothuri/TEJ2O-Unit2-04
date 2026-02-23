da008ed9926c38f01eb69244a902dff7965367f9
murali-kothuri <murali.kothuri@stu.ocsb.ca>
2026-02-23 12:43:51 -0500

    change and commit

--- main.ts
+++ main.ts
@@ -1,19 +1,16 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Murali Kothuri
- * Created on: Feb 2026
- * This program simulates the current temperature
-*/
+/* Copyright (c) 2020 MTHS All rights reserved *
+* Created by: Murali Kothuri *
+* Created on: Feb 2026 *
+* This program simulates the current temperature */
 
-// our variable for a random number
 let currentTemperature = 0
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-    let currentTemperature = (1, 100)
-    basic.showString("The temperature is:")
-    basic.showNumber(input.temperature())
-    basic.showString("C")
+    currentTemperature = randint(1, 100)
+    basic.showString('The temperature is:')
+    basic.showNumber(currentTemperature)
+    basic.showString('C')
 })
